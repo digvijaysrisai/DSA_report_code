b047ad053b6e218b13672d45e2622f25e6c71732
digvijaysrisai <57456068+digvijaysrisai@users.noreply.github.com>
2020-11-24 22:07:28 +0530

    Add files via upload

---
+++ create.c
@@ -0,0 +1,36 @@
+void createList(int n)
+{
+    int i, data;
+    struct node *newNode;
+
+    if (n >= 1)
+    {
+        head = (struct node *)malloc(sizeof(struct node));
+
+        printf("Enter data of 1 node: ");
+        scanf("%d", &data);
+
+        head->data = data;
+        head->prev = NULL;
+        head->next = NULL;
+
+        last = head;
+
+        for (i = 2; i <= n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+
+            printf("Enter data of %d node: ", i);
+            scanf("%d", &data);
+
+            newNode->data = data;
+            newNode->prev = last;
+            newNode->next = NULL;
+
+            last->next = newNode;
+            last = newNode;
+        }
+
+        printf("\nDOUBLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
